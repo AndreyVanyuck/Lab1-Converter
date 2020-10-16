9f40e1155819196f12238787a0c4a3659506dd26
AndreyVanyuck <avanyuck@gmail.com>
2020-10-16 22:57:54 +0300

    added interaction between fragments

--- app/src/main/java/com/example/converter/ConverterActivity.java
+++ app/src/main/java/com/example/converter/ConverterActivity.java
@@ -1,14 +1,38 @@
 package com.example.converter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class ConverterActivity extends AppCompatActivity {
+
+public class ConverterActivity extends AppCompatActivity implements NumPadFragment.OnFragmentInteractionListener {
+    NumPadFragment numPadFragment;
+    DataFragment dataFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_converter);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        numPadFragment = new NumPadFragment();
+        dataFragment = new DataFragment();
+        fragmentTransaction.add(R.id.numPadFragment,numPadFragment);
+        fragmentTransaction.add(R.id.dataFragment, dataFragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(String link) {
+        DataFragment fragment = (DataFragment) getSupportFragmentManager().findFragmentById(R.id.dataFragment);
+        fragment.setText(link);
     }
 }
--- app/src/main/java/com/example/converter/NumPadFragment.java
+++ app/src/main/java/com/example/converter/NumPadFragment.java
@@ -1,64 +1,70 @@
 package com.example.converter;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NumPadFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class NumPadFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private OnFragmentInteractionListener mListener;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NumPadFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment num_pad.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NumPadFragment newInstance(String param1, String param2) {
-        NumPadFragment fragment = new NumPadFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    interface OnFragmentInteractionListener {
+        void onFragmentInteraction(String link);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mListener = (OnFragmentInteractionListener) context;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_num_pad, container, false);
+        View view = inflater.inflate(R.layout.fragment_num_pad, container, false);
+
+        SetDigitClickListener(view.findViewById(R.id.button0));
+        SetDigitClickListener(view.findViewById(R.id.button1));
+        SetDigitClickListener(view.findViewById(R.id.button2));
+        SetDigitClickListener(view.findViewById(R.id.button3));
+        SetDigitClickListener(view.findViewById(R.id.button4));
+        SetDigitClickListener(view.findViewById(R.id.button5));
+        SetDigitClickListener(view.findViewById(R.id.button6));
+        SetDigitClickListener(view.findViewById(R.id.button7));
+        SetDigitClickListener(view.findViewById(R.id.button8));
+        SetDigitClickListener(view.findViewById(R.id.button9));
+        SetDigitClickListener(view.findViewById(R.id.buttonDot));
+        SetDigitClickListener(view.findViewById(R.id.buttonC));
+
+        return view;
+    }
+    private void SetDigitClickListener(Button button){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textButton = button.getText().toString();
+                mListener.onFragmentInteraction(textButton);
+            }
+        });
+    }
+
+    public void clickDigitButton(View view){
+        Button button = (Button) view;
+        String textButton = button.getText().toString();
     }
 }
